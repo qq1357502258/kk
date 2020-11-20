9420e36a8940a416d532268eff3ca3f643310d95
卢威 <luwei.reed@bytedance.com>
2020-11-20 17:28:45 +0800

    luwei add files

---
+++ Magina/Magina/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  Magina
+//
+//  Created by liujinze on 2020/10/26.
+//
+
+#import "AppDelegate.h"
+#import "MAGRootViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    MAGRootViewController *rootVC = [[MAGRootViewController alloc] init];
+    self.window.rootViewController = rootVC;
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:rootVC];
+    nav.navigationBarHidden = YES;
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
